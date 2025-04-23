2550d6e534fd9f1f82fcdb6fb3c7942bc6f45328
lucas.sodre <Lucas.Sodre@sovos.com>
2025-04-23 12:42:45 -0300

    Added Presentation Controllers

---
+++ BlogApi/IoC/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using BlogApi.Application;
+using BlogApi.Domain.Interfaces;
+using BlogApi.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogApi.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddBlogApiServices(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IBlogPostRepository, BlogPostRepository>();
+            services.AddScoped<BlogService>();
+
+            return services;
+        }
+    }
+}
