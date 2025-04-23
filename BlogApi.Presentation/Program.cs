2306d285be1c66dc3c66010c4ceb4a968245247e
Lucas Juan <ljgsodre@outlook.com>
2025-04-23 20:17:35 -0300

    Configuring JsonOptions

--- BlogApi.Presentation/Program.cs
+++ BlogApi.Presentation/Program.cs
@@ -3,15 +3,20 @@ using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+        options.JsonSerializerOptions.WriteIndented = true; 
+    });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
 builder.Services.AddBlogApiServices(builder.Configuration);
+
+
 
 Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
